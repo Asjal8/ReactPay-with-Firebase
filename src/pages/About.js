0202b3f52f31f413d9c76ebf1fc243e6f0598f9d
Asjal Abrar <asjalabrar8@gmail.com>
2022-11-03 16:26:53 +0500

    Third Commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import AboutUs from "../components/AboutUs";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Navbar />
+      <AboutUs />
+      <Footer />
+    </>
+  );
 };
 
 export default About;
